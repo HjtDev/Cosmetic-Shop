218d8d452919eec4b00015bca8742e57c8077a47
HjtDev <ronaldohjt2@gmail.com>
2024-10-15 19:02:44 +0330

    feat(order)
    Added order model
    Added order item model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from account.models import User
+from product.models import Product
+from django_jalali.db import models as jmodels
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = 'در صف برسی', _('در صف برسی')  # Pending for review
+        CONFIRMED = 'تایید سفارش', _('تایید سفارش')  # Order confirmed
+        PREPARING = 'آماده سازی سفارش', _('آماده سازی سفارش')  # Preparing order
+        SHIPPED = 'تحویل به پست', _('تحویل به پست')  # Shipped to post
+        DELIVERED = 'تحویل مرسوله به مشتری', _('تحویل مرسوله به مشتری')  # Delivered
+        DONE = 'تکمیل شد', _('تکمیل شد')
+
+    objects = jmodels.jManager
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.SET_NULL, null=True,
+                             verbose_name='کاربر')
+    first_name = models.CharField(max_length=50, verbose_name='نام')
+    last_name = models.CharField(max_length=50, verbose_name='نام خانوادگی')
+    email = models.EmailField(verbose_name='ایمیل', blank=True, null=True)
+    phone = models.CharField(max_length=11, verbose_name='تلفن')
+    address = models.CharField(max_length=300, verbose_name='آدرس')
+    postal_code = models.CharField(max_length=10, verbose_name='کد پستی')
+    province = models.CharField(max_length=50, verbose_name='استان')
+    city = models.CharField(max_length=50, verbose_name='شهر')
+    created_at = jmodels.jDateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = jmodels.jDateTimeField(auto_now=True, verbose_name='آخرین تغییر')
+    status = models.CharField(max_length=21, choices=OrderStatus.choices,
+                              default=OrderStatus.PENDING, verbose_name='وضعیت')
+    paid = models.BooleanField(default=False, verbose_name='پرداخت شده')
+
+    class Meta:
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارش ها'
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at']),
+        ]
+
+    def __str__(self):
+        return f'Order-{self.id}: {self.first_name} {self.last_name}'
+
+    def get_items_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE, verbose_name='سفارش')
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE, verbose_name='محصول')
+    price = models.PositiveIntegerField(default=0, verbose_name='قیمت')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='تعداد')
+
+    def __str__(self):
+        return f'OrderItem-{self.id}: {self.product}'
+
+    def get_cost(self):
+        return self.price * self.quantity
