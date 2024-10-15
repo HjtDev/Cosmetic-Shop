752f5c2bc34eaae45de314e6e9f6a85dc9d9579a
HjtDev <ronaldohjt2@gmail.com>
2024-10-15 19:30:46 +0330

    feat(order admin)
    Added admin view for the order and orderItem(stackedInline)

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
-# Register your models here.
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 1
+    verbose_name = 'محصول'
+    verbose_name_plural = 'محصولات'
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_id',
+        'user',
+        'phone',
+        'province',
+        'city',
+        'created_at',
+        'status',
+        'paid'
+    )
+    list_filter = (
+        'user',
+        'status',
+        'paid',
+        ('created_at', JDateFieldListFilter),
+        ('updated_at', JDateFieldListFilter)
+    )
+    search_fields = (
+        'order_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'address',
+        'postal_code',
+        'province',
+        'city',
+    )
+    list_editable = (
+        'status',
+        'paid'
+    )
+    ordering = ('created_at', 'updated_at')
+    inlines = (OrderItemInline,)
