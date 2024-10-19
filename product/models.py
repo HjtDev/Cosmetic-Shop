f69b0a643f5ba6c3072803c14f5f7675f51b0e22
HjtDev <ronaldohjt2@gmail.com>
2024-10-19 16:28:04 +0330

    feat(comment rating system)

--- product/models.py
+++ product/models.py
@@ -112,6 +112,7 @@ class Comment(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name='محصول')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', verbose_name='کاربر')
     text = models.TextField(verbose_name='متن', max_length=300)
+    rating = models.PositiveIntegerField(verbose_name='امتیاز', validators=[MaxValueValidator(5)], default=1)
     is_visible = models.BooleanField(verbose_name='نمایش در سایت', default=False)
 
     created_at = jmodels.jDateTimeField(verbose_name='تاریخ ایجاد', auto_now_add=True, editable=False)
