8c3856ce82f0386856d3ba9fe5eb7b54c55c7951
HjtDev <ronaldohjt2@gmail.com>
2024-10-19 11:05:32 +0330

    feat(base-checkout-page)

--- order/views.py
+++ order/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def checkout_view(request):
+    if request.session.get('cart', None).get('products', None):
+        return render(request, 'product-checkout.html')
+    return redirect('cart:cart_list')
